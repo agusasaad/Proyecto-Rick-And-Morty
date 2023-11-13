4d32680f15115978990a3b0ccceae4b754801d01
agusasaad <agusasaad1099@hotmail.com>
2023-11-13 10:30:01 -0300

    first commint

---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from './Card';
+
+export default function Cards(props) {
+   return <div className='Cards'>
+      {props.characters.map((character) => {
+         return (
+            <Card
+               key={character.id}
+               name={character.name}
+               status={character.status}
+               species={character.species}
+               gender={character.gender}
+               origin={character.origin.name}
+               image={character.image}
+               onClose={() => window.alert('Emulamos que se cierra la card')}
+            />
+         )
+      })}
+   </div>
+}
